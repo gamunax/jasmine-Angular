2de44d59f17541ea59319359c07fc7627088aefc
gamunax <gamunaxx@gmail.com>
2020-01-12 18:13:09 -0500

    initial commit

---
+++ src/app/04-forms/todo-form.component.spec.ts
@@ -0,0 +1,20 @@
+
+import { FormBuilder } from '@angular/forms';
+import { TodoFormComponent } from './todo-form.component';
+
+describe('TodoFormComponent', () => {
+  let component: TodoFormComponent;
+
+  beforeEach(() => {
+    component = new TodoFormComponent(new FormBuilder());
+
+  });
+
+  it('should create a form with 2 constrols', () => {
+    expect(component.form.contains('name')).toBeTruthy();
+    expect(component.form.contains('email')).toBeTruthy();
+  });
+
+  it('', () => {
+  });
+});
